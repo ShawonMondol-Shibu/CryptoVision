41432e69831840db22f4a599cb0616815ebd48f5
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-07 23:56:34 +0600

    dark/light mode added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
+'use client'
 import Image from "next/image";
-
+import { useEffect, useState } from "react";
+interface coinType{
+  id:string;
+  symbol:string;
+  name:string;
+}
 export default function Home() {
+  const [coins, setCoins] = useState<coinType[]>();
+  useEffect(()=>{
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/list?x-cg-demo-api-key=${process.env.CG_API_KEY}`
+    )
+    .then((res) => res.json())
+    .then((data) => setCoins(data));
+  },[])
+  console.log(coins);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {coins?.slice(0,10).map((coin) => (
+          <div key={coin.id}>
+            <p>
+
+            {coin.id}
+            </p>
+            <p>
+
+          {coin.name}
+            </p>
+            <p>
+
+          {coin.symbol}
+            </p>
+          </div>
+        ))}
         <Image
           className="dark:invert"
           src="/next.svg"
