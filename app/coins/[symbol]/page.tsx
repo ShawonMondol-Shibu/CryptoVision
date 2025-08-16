0eb09b95e8932c6622ee54052d00ac2f0cd49054
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-17 00:34:44 +0600

    coin symbol page updated

--- app/coins/[symbol]/page.tsx
+++ app/coins/[symbol]/page.tsx
@@ -1,5 +1,9 @@
 import CoinDetailPage from "../coin-detail-page"
 
-export default function CoinDetail({ params }: { params: { symbol: string } }) {
+export default function CoinDetail({
+  params,
+}: {
+  params: { symbol: string }
+}) {
   return <CoinDetailPage symbol={params.symbol} />
 }
